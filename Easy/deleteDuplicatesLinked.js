869fd3a00b9e05b50e5b13818b1835e8699ecd1a
Konstantin Nikolaev <you@example.com>
2023-04-10 20:45:38 -0700

    easy problems

--- Easy/deleteDuplicatesLinked.js
+++ Easy/deleteDuplicatesLinked.js
@@ -18,24 +18,17 @@ function deleteDuplicatesLinkedList(head) {
   if (!head) return head;
   if (head.val && !head.next) return head;
 
-  let currentNode = head;
-  let sortedList = new ListNode();
-  let start = sortedList;
+  let prevNode = head;
+  let currentNode = prevNode.next;
 
-  while (currentNode.next) {
-    if (currentNode.val === currentNode.next.val) {
+  while (currentNode) {
+    if (prevNode.val === currentNode.val) {
+      prevNode.next = currentNode.next;
       currentNode = currentNode.next;
-      if (!currentNode.next) sortedList.next = currentNode;
     } else {
-      if (!sortedList) {
-        sortedList = currentNode;
-        currentNode = currentNode.next;
-      } else {
-        sortedList.next = currentNode;
-        sortedList = sortedList.next;
-        currentNode = currentNode.next;
-      }
+      prevNode = currentNode;
+      currentNode = currentNode.next;
     }
   }
-  return start.next;
+  return head;
 }
