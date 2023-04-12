188eb55537abf9302313b559ae7b748ca6d9166c
Konstantin Nikolaev <you@example.com>
2023-04-11 18:27:58 -0700

    easy problems

--- Easy/mySqrt.js
+++ Easy/mySqrt.js
@@ -8,3 +8,21 @@ function mySqry(x) {
   }
   return i - 1;
 }
+
+function mySqrt(x) {
+  if (x === 0) return x;
+
+  let first = 1,
+    last = x;
+  while (first <= last) {
+    let mid = Math.floor(first + (last - first) / 2);
+    if (mid * mid === x) {
+      return mid;
+    } else if (mid * mid > x) {
+      last = mid - 1;
+    } else {
+      first = mid + 1;
+    }
+  }
+  return last;
+}
